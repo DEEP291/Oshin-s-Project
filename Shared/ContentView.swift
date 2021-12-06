85a82f5ce1f678a7ed267336ff5c9a5442b6397c
DEEP <srandeepkiran@gmail.com>
2021-12-06 15:35:42 -0500

    Text is Updated.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Its my first repoisitiory in x code")
             .padding()
     }
 }
