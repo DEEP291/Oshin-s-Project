bcf073ec1225d0038845d0da24bbf576c5cc09ab
DEEP <srandeepkiran@gmail.com>
2021-12-06 15:17:38 -0500

    Initial Commit

---
+++ Shared/Oshin_s_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oshin_s_ProjectApp.swift
+//  Shared
+//
+//  Created by kiran deep on 2021-12-06.
+//
+
+import SwiftUI
+
+@main
+struct Oshin_s_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
